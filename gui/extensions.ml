c167738c1f5105f2190aee007c9c274018e17c5c
Jma353 <jma353@cornell.edu>
2016-11-25 23:06:12 -0500

    Register drawing works (needs refactoring)

---
+++ gui/extensions.ml
@@ -0,0 +1,4 @@
+open D3
+
+let translate x y =
+  "translate(" ^ (string_of_int x) ^ "," ^ (string_of_int y) ^ ")"
---
+++ gui/extensions.mli
@@ -0,0 +1,5 @@
+open D3
+
+(* [translate x y] expresses the "transform" command of translation,
+ * given an x and a y. *)
+val translate : int -> int -> string
