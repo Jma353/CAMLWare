e9124172545fd2ebb1daaffc087f9e910a33bb78
Jma353 <jma353@cornell.edu>
2016-11-26 02:14:09 -0500

    Module separation

---
+++ components.mli
@@ -0,0 +1,4 @@
+open D3
+
+(* Register view component *)
+val register : int -> int -> ('a, 'b) D3.t -> ('a, 'b) D3.t
--- gui.ml
+++ gui.ml
@@ -1,45 +1,11 @@
 open D3
 open Extensions
+open Components
 open Circuit
 
 (* Dimensions of the window *)
 type dims =
   { width : int; height : int }
-
-(* Formatting helper *)
-
-(* Register view component *)
-let register w h svg =
-  let frame =
-    append "rect"
-    |. int attr "width" w
-    |. int attr "height" h
-    |. int attr "x" 0
-    |. int attr "y" 0
-    |. str style "stroke" "black"
-    |. str style "fill" "transparent"
-    |. int style "stroke-width" 1
-  in
-  let line1 =
-    append "line"
-    |. int attr "x1" 0
-    |. int attr "y1" (int_of_float (0.75 *. float_of_int h))
-    |. int attr "x2" (int_of_float (0.15 *. float_of_int w))
-    |. int attr "y2" (int_of_float (0.80 *. float_of_int h))
-    |. int style "stroke-width" 1
-    |. str style "stroke" "black"
-  in
-  let line2 =
-    append "line"
-    |. int attr "x1" (int_of_float (0.15 *. float_of_int w))
-    |. int attr "y1" (int_of_float (0.80 *. float_of_int h))
-    |. int attr "x2" 0
-    |. int attr "y2" (int_of_float (0.85 *. float_of_int h))
-    |. int style "stroke-width" 1
-    |. str style "stroke" "black"
-  in
-  ((svg |- frame) |- line1) |- line2
-
 
 (* View info *)
 let view dims padding =
