9200863dc521dfc43c4287a2c0ef844cc82b8ae0
Jma353 <jma353@cornell.edu>
2016-11-27 15:21:12 -0500

    SVG border for circuit drawing

--- gui.ml
+++ gui.ml
@@ -12,17 +12,27 @@ type dims =
 let view dims padding =
   (* Base SVG *)
   let svg =
-    append "svg"
+    (append "svg"
     |. int attr "width"  dims.width
-    |. int attr "height" dims.height
-  in
+    |. int attr "height" dims.height) in
+  (* Global container *)
   let g =
-    append "g"
-    |. str attr "transform" (translate padding padding)
-  in
+    (append "g"
+    |. str attr "transform" (translate padding padding)) in
+
+  (* Border *)
+  let border_rect =
+    (append "rect"
+    |. int attr "x" 0
+    |. int attr "y" 0
+    |. int attr "width" dims.width
+    |. int attr "height" dims.height
+    |. str style "stroke" "black"
+    |. str style "fill" "none"
+    |. int style "stroke-width" 1) in
 
   (* Base container *)
-  let container = svg <.> g in
+  let container = (svg |- border_rect) <.> g in
 
   let one = arith_nxor 0. 50. 60. in
   let two = arith_nxor 0. 150. 40. in
@@ -43,4 +53,4 @@ let view dims padding =
 ;;
 
 let _ =
-  run ~node:(Dom_html.document##body) (view { width = 700; height = 700 } 50) ()
+  run ~node:(Dom_html.document##body) (view { width = 1000; height = 700 } 50) ()
